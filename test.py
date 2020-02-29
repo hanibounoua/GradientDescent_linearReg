1a50ec2f647f358b4944ea78bcd6a94c8d694d2c
Hani Bounoua <bounoua.hani@gmail.com>
2020-02-29 21:19:58 +0100

    final

--- test.py
+++ test.py
@@ -2,18 +2,15 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from linearRegression import linearRegression
 
-points = np.genfromtxt("data.csv", delimiter=",")
+def run():
+    points = np.genfromtxt("data.csv", delimiter=",")
+    X = points[:, 0]
+    y = points[:, 1]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .33, random_state = 123)
+    model = linearRegression(learningRate = .0001, n_iter = 1000)
+    model.fit(X_train, y_train)
+    y_predicted = model.predict(X_test)
+    return {"Model": model, "Mean Squared Error": np.mean((y_predicted - y_test)**2)}
 
-X = points[:, 0]
-y = points[:, 1]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .33, random_state = 123)
-
-model = linearRegression(learningRate = .0001, n_iter = 1000)
-model.fit(X_train, y_train)
-
-print(model.omega, model.b)
-
-y_predicted = model.predict(X_test)
-
-print(np.mean((y_predicted - y_test)**2))
+if __name__ == "main":
+    run()
